93d3611b6fff9d56a990ddea6418969f29a84bb2
Vladislav Pashkov <xelliks666@gmail.com>
2025-07-17 19:54:42 +0700

    Added classes for entityes

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/Customer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    internal class Customer
+    {
+        private static int idCounter = 1;
+        private readonly int _id;
+        private string _fullname;
+        private string _address;
+
+        public Customer(string fullname, string address)
+        {
+            _id  = idCounter++;
+            Fullname = fullname;
+            Address = address;
+        }
+
+        public string Fullname { get { return _fullname; } set
+            {
+                if (value.Length < 200)
+                {
+                    _fullname = value;
+                } else
+                {
+                    throw new ArgumentException();
+                }
+            } 
+        }
+        public string Address
+        {
+            get { return _address; }
+            set
+            {
+                if (value.Length < 500)
+                {
+                    _address = value;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+    }
+}
---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/Item.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    internal class Item
+    {
+        private static long idCounter = 0;
+        private readonly long _id;
+        private string _name;
+        private string _info;
+        private double _cost;
+
+        public Item(string name, double cost, string info)
+        {
+            Name = name;
+            Cost = cost;
+            Info = info;
+            _id = idCounter++;
+        }
+
+        public string Name { get { return _name; } set
+            {
+                if (value.Length < 200)
+                {
+                    _name = value;
+                } else
+                {
+                    throw new ArgumentException();
+                }
+            } }
+        public double Cost
+        {
+            get { return _cost; }
+            set
+            {
+                if (value >= 0 && value <= 100000)
+                {
+                    _cost = value;
+                } else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+        public string Info { get { return _info; } set
+            {
+                if (value.Length < 1000)
+                {
+                    _info = value; 
+                } else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+    }
+}
