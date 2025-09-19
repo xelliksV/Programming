1c99eabaa44f15c15f8b60023b969f56c92868fe
Vladislav Pashkov <xelliks666@gmail.com>
2025-09-19 07:46:27 +0700

    Added priority order

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/PriorityOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    public class PriorityOrder : Order
+    {
+        private DateTime _date;
+        private String _time;
+        public PriorityOrder(Address address, Cart cart, DateTime date, String time, String name) : base(address, cart, name)
+        {
+
+            _date = date;
+            _time = time;
+        }
+    }
+}
