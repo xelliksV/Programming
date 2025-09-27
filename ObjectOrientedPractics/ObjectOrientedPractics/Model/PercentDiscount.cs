26c88b9fd29fe9f1d90a4f08de7f6bdc8b71de30
Vladislav Pashkov <xelliks666@gmail.com>
2025-09-27 17:42:14 +0700

    Added discounts

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/PercentDiscount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    public class PercentDiscount
+    {
+        private Category category;
+        private double percent = 0.99;
+        private double total;
+
+        public string Info { get { return "Процентная " + category.ToString() + " - " + (1 - percent).ToString(); } }
+        private double isCategoryPresent(List<Item> items)
+        {
+            double total = 0;
+            foreach (Item item in items)
+            {
+                if (item.Category == category)
+                {
+                    total += item.Cost;
+                }
+            }
+            return total;
+        }
+        public double Calculate(List<Item> items)
+        {
+            double total = isCategoryPresent(items);
+            this.total += total;
+            return total * percent;
+        }
+        public double Apply(List<Item> items)
+        {
+            Update(items);
+            return Calculate(items);
+        }
+        public void Update(List<Item> items) 
+        {
+            if (isCategoryPresent(items) > 1000 && percent <= 0.9)
+            {
+                percent -= 0.01;
+            }
+        }
+    }
+}
---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/PointsDiscounts.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    public class PointsDiscounts
+    {
+        private int count;
+        public int Count { get { return count; } private set { } }
+        private double amount(List<Item> items)
+        {
+            double total = 0;
+            foreach (Item item in items)
+            {
+                total += item.Cost;
+            }
+            return total;
+        }
+        public double calculate(List<Item> items)
+        {
+            double total = amount(items);
+            if (count <= total * 0.3)
+            {
+                return count;
+            } else
+            {
+                return total * 0.3;
+            }
+        }
+        public double Apply(List<Item> items)
+        {
+            double result = calculate(items);
+            if (result == count)
+            {
+                count = 0;
+            } else
+            {
+                count -= (int) result;
+            }
+            return result;
+        }
+        public void Update(List<Item> items) 
+        {
+            count += (int) (amount(items) * 0.1);
+        }
+        public String info { get { return "Накопительная - " +  count + " баллов."; } }
+    }
+}
